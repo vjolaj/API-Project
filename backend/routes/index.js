dc8aec7d633bf5ae058a75e6a6c8d384a13041a0
vjolaj <vjolajorgji@gmail.com>
2023-05-10 16:37:52 -0400

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,14 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+
+router.use('/api', apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
@@ -15,5 +18,5 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
-  
+
 module.exports = router;
