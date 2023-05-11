4a14040565929e29419ee0141c04f251cb2a66e7
vjolaj <vjolajorgji@gmail.com>
2023-05-11 18:49:12 -0400

    Add User Authentication Express middleware

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,22 +1,23 @@
 // backend/routes/index.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const apiRouter = require('./api');
+const apiRouter = require("./api");
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
 
-router.use('/api', apiRouter);
+router.use("/api", apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
   });
+});
+
 
 module.exports = router;
