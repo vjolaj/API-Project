2682e5b1426ebd0f851df16aa0868617b8be256d
vjolaj <vjolajorgji@gmail.com>
2023-05-12 18:31:32 -0400

    Add firstName, lastName properties to User

--- backend/db/seeders/20230511203521-demo-user.js
+++ backend/db/seeders/20230511203521-demo-user.js
@@ -13,16 +13,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        firstName: "Demouser",
+        lastName: "One",
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: "Demouser",
+        lastName: "Two",
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: "Demouser",
+        lastName: "Three",
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
