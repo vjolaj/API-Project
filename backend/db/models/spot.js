312776f9d9f188adba6ff725fee9c810039d755a
vjolaj <vjolajorgji@gmail.com>
2023-05-18 17:47:17 -0700

    debug issues while testing

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Spot.hasMany(models.Review, {foreignKey: 'spotId', onDelete: 'CASCADE',  hooks: true})
       Spot.hasMany(models.SpotImage, {foreignKey: 'spotId', onDelete: 'CASCADE',  hooks: true})
-      Spot.belongsTo(models.User, {foreignKey: 'ownerId'})
+      Spot.belongsTo(models.User, {foreignKey: 'ownerId',  as: 'Owner'})
       Spot.hasMany(models.Booking, {foreignKey: 'spotId', onDelete: 'CASCADE',  hooks: true})
     }
   }
