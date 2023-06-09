e6b754c330a16b4c69aef2db9dd0c96763c4da63
vjolaj <vjolajorgji@gmail.com>
2023-06-09 13:51:31 -0400

    fix bug

--- backend/db/seeders/20230516173120-demo-booking.js
+++ backend/db/seeders/20230516173120-demo-booking.js
@@ -18,26 +18,26 @@ module.exports = {
           spotId: 1,
           userId: 2,
           startDate: "2021-10-10",
-          endDate: "2021-10-13",
+          endDate: "2021-10-13"
         },
         {
           spotId: 2,
           userId: 3,
           startDate: "2022-11-10",
-          endDate: "2022-11-17",
+          endDate: "2022-11-17"
         },
         {
           spotId: 3,
           userId: 1,
           startDate: "2022-04-15",
-          endDate: "2022-04-23",
+          endDate: "2022-04-23"
         },
         {
           spotId: 4,
           userId: 2,
           startDate: "2023-01-02",
-          endDate: "2023-01-06",
-        },
+          endDate: "2023-01-06"
+        }
       ],
       {}
     );
@@ -50,10 +50,10 @@ module.exports = {
       options,
       {
         startDate: {
-          [Op.eq]: ["2021-10-10", "2022-11-10", "2022-04-15", "2023-01-02"],
+          [Op.in]: ["2021-10-10", "2022-11-10", "2022-04-15", "2023-01-02"]
         },
       },
       {}
     );
-  },
+  }
 };
